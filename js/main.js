84fd9a0a3635580da6f1b25fb885d02a54034aad
Giuseppe <giuseppe.galati84@gmail.com>
2017-04-12 10:57:07 +0200

    update files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 
+        /* sticky navigation */
+
         $(".js--section-features").waypoint(function(direction){
 
             if (direction == "down"){
@@ -9,10 +11,41 @@ $(document).ready(function(){
                 $("nav").removeClass ("sticky")
 
             }
-        })
+        }, {
+            offset: "60px;"
+        });
 
 
+            /* Scroll on buttons */
+            $(".js--scroll-to-plans").click(function(){
+                $("html, body").animate({scrollTop:$(".js--section-plans").offset().top}, 1000);
+            });
+
+            $(".js--scroll-to-plans").click(function(){
+                $("html, body").animate({scrollTop:$(".js--section-features").offset().top}, 1000);
+            });
 
 
+             /* navigation scroll */
+             $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+            /* Animation */
+            $(".js--wp-1").waypoint(function(direction){
 
+                $(".js-wp")
+
+
+            })
 });
