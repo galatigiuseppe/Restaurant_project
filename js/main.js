3425b96346acbb43ea80123da23383650938a3e8
giuseppe galati <giuseppe.galati84@gmail.com>
2017-04-13 10:17:41 +0200

    update project

--- js/main.js
+++ js/main.js
@@ -62,14 +62,16 @@ $(document).ready(function(){
             });
             $(".js--wp-4").waypoint(function(direction){
 
-                $(".js--wp-4").addClass("animated fadeInUp");
+                $(".js--wp-4").addClass("animated pulse");
             }, {
                 offset: "50%"
             });
 
+                /* Mobile navigation */
+
            $(".js--nav-icon").click(function() {
                var nav = $(".js--main-nav");
-               var icon = $(".js--nav-icon ");
+               var icon = $(".js--nav-icon i ");
                
                nav.slideToggle(200);
                if (icon.hasClass("ion-navicon-round")){
